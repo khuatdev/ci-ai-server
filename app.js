5828733462bac5274a4c3e30df209e89b49fd851
hoangkm <hoang.manh.khuat@gmail.com>
2023-09-17 10:15:22 +0700

    Update: change ai port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import { main } from './gpt.js'; // Import the "main" function from index.js
 
 const app = express();
-const port = 3000;
+const port = 3030;
 
 const givenSample = `
 # Sample 1
